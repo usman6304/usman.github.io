96a02f67b9d1fa2e0f3410a23f28747e2e47da2b
usman6304 <usman_6304@yahoo.com>
2016-11-07 02:32:00 -0800

    Files Uploaded

---
+++ mywork.php
@@ -0,0 +1,258 @@
+<!doctype html><!--[if lt IE 7]>      
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="">
+   <![endif]--><!--[if IE 7]>         
+   <html class="no-js lt-ie9 lt-ie8" lang="">
+      <![endif]--><!--[if IE 8]>         
+      <html class="no-js lt-ie9" lang="">
+         <![endif]--><!--[if gt IE 8]><!-->
+         <html class="no-js" lang="">
+            <!--<![endif]-->
+            <head>
+               <meta charset="utf-8">
+               <title>UsMaN - Creative Resume Template</title>
+               <meta name="description" content="Resume">
+               <meta name="keywords" content="usman, resume,">
+               <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+               <link href="http://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" type="text/css">
+               <link href="http://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet" type="text/css">
+               <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+               <link href="css/icomoon.min.css" rel="stylesheet" type="text/css">
+               <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+               <link href="css/jpreloader.min.css" rel="stylesheet" type="text/css">
+               <link href="css/animate.min.css" rel="stylesheet" type="text/css">
+               <link href="css/magnific-popup.min.css" rel="stylesheet" type="text/css">
+               <link href="css/flexslider.min.css" rel="stylesheet" type="text/css">
+               <link href="css/style.min.css" rel="stylesheet" type="text/css">
+               <link href="css/switch-style.css" rel="stylesheet" type="text/css">
+               <!--[if lt IE 8]>
+               <script src="js/modernizr.min.js"></script>
+               <![endif]-->
+            </head>
+            <body oncontextmenu="return false" onselectstart="return false" ondrag="return false" id="page-top" data-spy="scroll" data-target=".navbar-fixed-top" data-offset="66">
+               <!--[if lt IE 10]>
+               <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+               <![endif]-->
+               <link id="switch_style" href="#" rel="stylesheet" type="text/css">
+               <header id="home" class="">
+               </header>
+               <nav id="navigation" class="navbar navbar-fixed-top center-menu" role="navigation">
+                  <div class="container navbar-container">
+                     <div class="navbar-header page-scroll"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".berg-collapse"><span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span></button> <a class="navbar-brand visible-xs visible-sm" href="#page-top">USMAN</a></div>
+                     <div class="navbar-collapse collapse berg-collapse">
+                        <ul class="nav navbar-nav">
+                           <li class="page-scroll"><a href="#home">Home</a></li>
+                           <li class="page-scroll"><a href="#profile">Profile</a></li>
+                           <li class="page-scroll"><a href="#portfolio">Portfolio</a></li>
+                           <li class="page-scroll"><a href="#services">Services</a></li>
+                           <li class="page-scroll"><a href="#resume">Resume</a></li>
+                           <li class="page-scroll"><a href="#references">References</a></li>
+                           <li class="page-scroll"><a href="#skills">Skills</a></li>
+                           <li class="page-scroll"><a href="#contact">Contact</a></li>
+                        </ul>
+                     </div>
+                  </div>
+               </nav>
+               <section id="portfolio" class="section">
+                  <div class="container-fluid section-wrapper">
+                     <div class="section-content">
+                        <div class="row">
+                           <div class="col-md-12 text-center">
+                              <h2 class="section-title">My Latest Work</h2>
+                              <span class="divider center"></span>
+                           </div>
+                        </div>
+                        <div class="row">
+                           <div class="col-md-12">
+                              <ul class="filterable-portfolio-nav list-inline text-center">
+                                
+                                 <li class="active"><a href="#" data-filter="*" class="btn btn-sm btn-custom disabled">All</a></li>
+                                 <li><a href="#" data-filter=".webdevelopment" class="btn btn-sm btn-custom">Web development</a></li>
+                                 <li><a href="#" data-filter=".webdesigns" class="btn btn-sm btn-custom">Wordpress</a></li>
+                                 <li><a href="#" data-filter=".bootstrap" class="btn btn-sm btn-custom"> Bootstrap</a></li>
+                                 <li><a href="#" data-filter=".responsive" class="btn btn-sm btn-custom">Responsive designs </a></li>
+                                 
+                              </ul>
+                           </div>
+                        </div>
+                        <div class="portfolio">
+                           <div class="row filterable-portfolio">
+                              <div class="item webdesigns  col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/shiuman-blog.png" class="zoom" title="Shiuman desing & print"><i class="icon-Full-Screen"></i> </a><a href="http://shiuman.com/shiuman-blog/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Shiuman desing & print</h4>
+                                    </div>
+                                    <center><img src="images/portfolio/shiuman-blog.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+                              <div class="item webdesigns  col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/ahsanullahenterprises.png" class="zoom" title="Ahsanullah enterprises"><i class="icon-Full-Screen"></i> </a><a href="http://www.ahsanullahenterprises.com" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Ahsanullah enterprises</h4>
+                                    </div>
+                                   <center> <img src="images/portfolio/ahsanullahenterprises.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+                              <div class="item  webdesigns webdevelopment responsive col-sm-6 col-md-3">
+                                 <div class="project-wrapper" >
+                                    <div class="project-link"><a href="images/portfolio/taaruff.png"  class="zoom" title="Taaruff"><i class="icon-Full-Screen"></i> </a><a href="http://www.taaruff.com" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Taaruff</h4>
+                                    </div>
+                                  <center>  <img src="images/portfolio/taaruff.png" alt=""  class="imgg"></center>
+                                 </div>
+                              </div>
+                              <div class="item webdesigns webdevelopment responsive col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/jamie.png" class="zoom" title="Jamie delaine Watson"><i class="icon-Full-Screen"></i> </a><a href="http://jamiedelainewatson.com/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Jamie delaine Watson</h4>
+                                    </div>
+                                    <center><img src="images/portfolio/jamie.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+                              <div class="item webdesigns col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/giftpalace.png" class="zoom" title="Gift Palace"><i class="icon-Full-Screen"></i> </a><a href="http://giftpalace.co/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Gift Palace</h4>
+                                    </div>
+                                   <center> <img src="images/portfolio/giftpalace.png" alt="" class="imgg"></center>
+                              </div>
+							  </div>
+                              <div class="item webdesigns digital-art col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/nelson.png" class="zoom" title="NILSON AND PETRA"><i class="icon-Full-Screen"></i> </a><a href="http://www.nilsonandpetra.co.uk/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>NILSON AND PETRA </h4>
+                                    </div>
+                                    <center><img src="images/portfolio/nelson.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+                              <div class="item webdesigns col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/films.png" class="zoom" title="Fernden Santa Films "><i class="icon-Full-Screen"></i> </a><a href="http://www.ferndensantafilms.co.uk/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Fernden Santa Films </h4>
+                                    </div>
+                                    <center><img src="images/portfolio/films.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+                              <div class="item webdesigns col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/toldhom.png" class="zoom" title="Todholm Ltd"><i class="icon-Full-Screen"></i> </a><a href="http://www.todholm.co.uk/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Todholm Ltd</h4>
+                                    </div>
+                                    <center><img src="images/portfolio/toldhom.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+							   <div class="item webdesigns col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/watson.png" class="zoom" title="Watson Jones Investments Ltd"><i class="icon-Full-Screen"></i> </a><a href="http://www.watsonjonesinvestments.co.uk/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Watson Jones Investments Ltd</h4>
+                                    </div>
+                                    <center><img src="images/portfolio/watson.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+							  <div class="item webdesigns col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/pentra.png" class="zoom" title="Pentara Developments Ltd"><i class="icon-Full-Screen"></i> </a><a href="http://www.pentaradevelopments.co.uk/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Pentara Developments Ltd</h4>
+                                    </div>
+                                    <center><img src="images/portfolio/pentra.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+							  <div class="item webdesigns col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/zamhtech.png" class="zoom" title="Zamhtech(Currently Working for)"><i class="icon-Full-Screen"></i> </a><a href="http://www.zamhtech.com/" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Zamhtech</h4>
+									   
+                                    </div>
+                                    <center><img src="images/portfolio/zamhtech.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+							   <div class="item webdevelopment col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/easyrecharge.png" class="zoom" title="Easy Recharge System"><i class="icon-Full-Screen"></i> </a><a href="images/portfolio/easyrecharge.png" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Easy Recharge</h4>
+                                    </div>
+                                    <center><img src="images/portfolio/easyrecharge.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+							  <div class="item webdevelopment  col-sm-6 col-md-3">
+                                 <div class="project-wrapper">
+                                    <div class="project-link"><a href="images/portfolio/students.fw.png" class="zoom" title="Student Registration System"><i class="icon-Full-Screen"></i> </a><a href="images/portfolio/students.fw.png" target="_blank" class="external-link"><i class="icon-Link"></i></a></div>
+                                    <div class="project-title">
+                                       <h4>Student Registration System</h4>
+                                    </div>
+                                   <center> <img src="images/portfolio/students.fw.png" alt="" class="imgg"></center>
+                                 </div>
+                              </div>
+                           </div>	
+								<div class="clearfix"></div>
+
+						   <br>
+                     </div>
+                        </div>
+						
+                  </div>
+               </section>
+               
+              <br><br>
+               <footer class="footer">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="page-scroll"><a href="#page-top"><i class="icon-Arrow-Up"></i></a></div>
+                           <ul class="list-inline social-icons">
+                              <li>
+                                 <div class="item"><a href="https://www.facebook.com/u.6304" target="_blank"><i class="fa fa-facebook"></i></a></div>
+                              </li>
+                              <li>
+                                 <div class="item"><a href="https://twitter.com/usman_6304" target="_blank"><i class="fa fa-twitter"></i></a></div>
+                              </li>
+                              <li>
+                                 <div class="item"><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></div>
+                              </li>
+                              <li>
+                                 <div class="item"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></div>
+                              </li>
+                              
+                           </ul>
+                        </div>
+                     </div>
+                     <div class="row">
+                        <div class="col-md-12">
+                           <div class="copyright text-center">
+                              <p class="text-grey">© 2016. All rights reserved.</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </footer>
+               <script src="js/jquery.min.js"></script><script src="js/bootstrap.min.js"></script><script src="js/detectmobilebrowser.min.js"></script><script src="js/plugins.min.js"></script><script src="js/main.min.js"></script><script src="js/filterable-portfolio.min.js"></script><script src="js/animation.min.js"></script><script src="js/switch-style.js"></script>
+            </body>
+         </html>
+         <script>
+		 $(document).ready(function(){
+			$('#form').hide();
+			$('#con').click(function(){
+			$('#toslide').slideUp(5000);
+			$('#form').slideToggle(5000);
+			});
+		 });
+             $(document).bind("contextmenu",function(e) {
+                        e.preventDefault();
+});
+             $(document).keydown(function(e){
+                if(e.which === 123){
+                return false;
+    }
+});
+        </script>
